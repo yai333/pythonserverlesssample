6808b9f785e42872f77b4bbf2b75fa320e7064b7
yai333 <yi.ai@neaminational.org.au>
2019-03-05 18:17:03 +1100

    add lambda hanlder function

--- handler.py
+++ handler.py
@@ -2,143 +2,44 @@ import json
 import boto3
 import os
 import pandas as pd
+import asyncio
+import logging
 
+dbClient = boto3.client('dynamodb')
+s3 = boto3.client('s3')
 
-client = boto3.client('dynamodb')
 
 def importCSVToDB(event, context):
     documentsTable = os.environ['documentsTable']
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    file_key = event['Records'][0]['s3']['object']['key']
 
-    df =pd.read_csv('afs_lic_201902.csv', skip_blank_lines=True,
-                    usecols=['REGISTER_NAME', 'AFS_LIC_NUM', 'AFS_LIC_NAME', 'AFS_LIC_ADD_LOCAL',
-                             'AFS_LIC_ADD_STATE', 'AFS_LIC_ADD_PCODE'])
+    logger.info('Reading {} from {}'.format(file_key, bucket_name))
+
+    csv = s3.get_object(Bucket=bucket_name, Key=file_key)
+
+    df = pd.read_csv(csv, skip_blank_lines=True,
+                     usecols=['REGISTER_NAME', 'AFS_LIC_NUM', 'AFS_LIC_NAME', 'AFS_LIC_ADD_LOCAL',
+                              'AFS_LIC_ADD_STATE', 'AFS_LIC_ADD_PCODE'])
 
     df = df.astype(str)
     df = df.fillna("NA")
     values = df.T.to_dict().values()
 
     for value in values:
-        print(value)
-        client.update_item(
-            TableName=documentsTable,
-            Key={
-                'string': {
-                    value.AFS_LIC_NUM
-                }
-            },
-            AttributeUpdates={
-                'string': {
-                    'Value': {
-                        'S': 'string',
-                        'N': 'string',
-                        'B': b'bytes',
-                        'SS': [
-                            'string',
-                        ],
-                        'NS': [
-                            'string',
-                        ],
-                        'BS': [
-                            b'bytes',
-                        ],
-                        'M': {
-                            'string': {'... recursive ...'}
-                        },
-                        'L': [
-                            {'... recursive ...'},
-                        ],
-                        'NULL': True | False,
-                        'BOOL': True | False
-                    },
-                    'Action': 'ADD' | 'PUT' | 'DELETE'
-                }
-            },
-            Expected={
-                'string': {
-                    'Value': {
-                        'S': 'string',
-                        'N': 'string',
-                        'B': b'bytes',
-                        'SS': [
-                            'string',
-                        ],
-                        'NS': [
-                            'string',
-                        ],
-                        'BS': [
-                            b'bytes',
-                        ],
-                        'M': {
-                            'string': {'... recursive ...'}
-                        },
-                        'L': [
-                            {'... recursive ...'},
-                        ],
-                        'NULL': True | False,
-                        'BOOL': True | False
-                    },
-                    'Exists': True | False,
-                    'ComparisonOperator': 'EQ' | 'NE' | 'IN' | 'LE' | 'LT' | 'GE' | 'GT' | 'BETWEEN' | 'NOT_NULL' | 'NULL' | 'CONTAINS' | 'NOT_CONTAINS' | 'BEGINS_WITH',
-                    'AttributeValueList': [
-                        {
-                            'S': 'string',
-                            'N': 'string',
-                            'B': b'bytes',
-                            'SS': [
-                                'string',
-                            ],
-                            'NS': [
-                                'string',
-                            ],
-                            'BS': [
-                                b'bytes',
-                            ],
-                            'M': {
-                                'string': {'... recursive ...'}
-                            },
-                            'L': [
-                                {'... recursive ...'},
-                            ],
-                            'NULL': True | False,
-                            'BOOL': True | False
-                        },
-                    ]
-                }
-            },
-            ConditionalOperator='AND' | 'OR',
-            ReturnValues='NONE' | 'ALL_OLD' | 'UPDATED_OLD' | 'ALL_NEW' | 'UPDATED_NEW',
-            ReturnConsumedCapacity='INDEXES' | 'TOTAL' | 'NONE',
-            ReturnItemCollectionMetrics='SIZE' | 'NONE',
-            UpdateExpression='string',
-            ConditionExpression='string',
-            ExpressionAttributeNames={
-                'string': 'string'
-            },
-            ExpressionAttributeValues={
-                'string': {
-                    'S': 'string',
-                    'N': 'string',
-                    'B': b'bytes',
-                    'SS': [
-                        'string',
-                    ],
-                    'NS': [
-                        'string',
-                    ],
-                    'BS': [
-                        b'bytes',
-                    ],
-                    'M': {
-                        'string': {'... recursive ...'}
-                    },
-                    'L': [
-                        {'... recursive ...'},
-                    ],
-                    'NULL': True | False,
-                    'BOOL': True | False
-                }
-            }
+        if value["AFS_LIC_NUM"]:
+            last_updated = value["AFS_LIC_NUM"]
+            dbClient.update_item(
+                TableName=documentsTable,
+                Key={
+                    'id': {"S": value["AFS_LIC_NUM"]}
+                },
+                UpdateExpression="set reg_name = :r",
+                ExpressionAttributeValues={
+                    ':r': {"S": value['REGISTER_NAME']},
+                },
+                ReturnValues="UPDATED_NEW"
 
-    #with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            )
 
-
+    logger.info('last updated value {}'.format(last_updated))
---
+++ node_modules/serverless-python-requirements/unzip_requirements.py
@@ -0,0 +1,21 @@
+import os
+import shutil
+import sys
+import zipfile
+
+
+pkgdir = '/tmp/sls-py-req'
+
+sys.path.append(pkgdir)
+
+if not os.path.exists(pkgdir):
+    tempdir = '/tmp/_temp-sls-py-req'
+    if os.path.exists(tempdir):
+        shutil.rmtree(tempdir)
+
+    default_lambda_task_root = os.environ.get('LAMBDA_TASK_ROOT', os.getcwd())
+    lambda_task_root = os.getcwd() if os.environ.get('IS_LOCAL') == 'true' else default_lambda_task_root
+    zip_requirements = os.path.join(lambda_task_root, '.requirements.zip')
+
+    zipfile.ZipFile(zip_requirements, 'r').extractall(tempdir)
+    os.rename(tempdir, pkgdir)  # Atomic
